1a9d7aa06ffa0536291ef0c940f3ce9f7d33c185
Mubariz Afzal <42556114+mubarizafzal@users.noreply.github.com>
2022-11-14 13:33:36 -0500

    add navigation sequence for home page

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,7 +23,87 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.NavSettingsBtn = New System.Windows.Forms.Button()
+        Me.CallAssistanceBtn = New System.Windows.Forms.Button()
+        Me.WheelchairBtn = New System.Windows.Forms.Button()
+        Me.DoorOpsBtn = New System.Windows.Forms.Button()
+        Me.VisitAptsBtn = New System.Windows.Forms.Button()
+        Me.EnvCtrlBtn = New System.Windows.Forms.Button()
+        Me.CommBtn = New System.Windows.Forms.Button()
+        Me.ExitBtn = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'NavSettingsBtn
+        '
+        Me.NavSettingsBtn.Location = New System.Drawing.Point(110, 115)
+        Me.NavSettingsBtn.Name = "NavSettingsBtn"
+        Me.NavSettingsBtn.Size = New System.Drawing.Size(100, 80)
+        Me.NavSettingsBtn.TabIndex = 0
+        Me.NavSettingsBtn.Text = "Navigation Settings"
+        Me.NavSettingsBtn.UseVisualStyleBackColor = True
+        '
+        'CallAssistanceBtn
+        '
+        Me.CallAssistanceBtn.Location = New System.Drawing.Point(235, 115)
+        Me.CallAssistanceBtn.Name = "CallAssistanceBtn"
+        Me.CallAssistanceBtn.Size = New System.Drawing.Size(100, 80)
+        Me.CallAssistanceBtn.TabIndex = 1
+        Me.CallAssistanceBtn.Text = "Call For Assistance"
+        Me.CallAssistanceBtn.UseVisualStyleBackColor = True
+        '
+        'WheelchairBtn
+        '
+        Me.WheelchairBtn.Location = New System.Drawing.Point(360, 115)
+        Me.WheelchairBtn.Name = "WheelchairBtn"
+        Me.WheelchairBtn.Size = New System.Drawing.Size(100, 80)
+        Me.WheelchairBtn.TabIndex = 2
+        Me.WheelchairBtn.Text = "Wheelchair"
+        Me.WheelchairBtn.UseVisualStyleBackColor = True
+        '
+        'DoorOpsBtn
+        '
+        Me.DoorOpsBtn.Location = New System.Drawing.Point(485, 115)
+        Me.DoorOpsBtn.Name = "DoorOpsBtn"
+        Me.DoorOpsBtn.Size = New System.Drawing.Size(100, 80)
+        Me.DoorOpsBtn.TabIndex = 3
+        Me.DoorOpsBtn.Text = "Door Operations"
+        Me.DoorOpsBtn.UseVisualStyleBackColor = True
+        '
+        'VisitAptsBtn
+        '
+        Me.VisitAptsBtn.Location = New System.Drawing.Point(610, 115)
+        Me.VisitAptsBtn.Name = "VisitAptsBtn"
+        Me.VisitAptsBtn.Size = New System.Drawing.Size(100, 80)
+        Me.VisitAptsBtn.TabIndex = 4
+        Me.VisitAptsBtn.Text = "Visit Apartments"
+        Me.VisitAptsBtn.UseVisualStyleBackColor = True
+        '
+        'EnvCtrlBtn
+        '
+        Me.EnvCtrlBtn.Location = New System.Drawing.Point(735, 115)
+        Me.EnvCtrlBtn.Name = "EnvCtrlBtn"
+        Me.EnvCtrlBtn.Size = New System.Drawing.Size(100, 80)
+        Me.EnvCtrlBtn.TabIndex = 5
+        Me.EnvCtrlBtn.Text = "Environment Control"
+        Me.EnvCtrlBtn.UseVisualStyleBackColor = True
+        '
+        'CommBtn
+        '
+        Me.CommBtn.Location = New System.Drawing.Point(110, 230)
+        Me.CommBtn.Name = "CommBtn"
+        Me.CommBtn.Size = New System.Drawing.Size(100, 80)
+        Me.CommBtn.TabIndex = 6
+        Me.CommBtn.Text = "Communication"
+        Me.CommBtn.UseVisualStyleBackColor = True
+        '
+        'ExitBtn
+        '
+        Me.ExitBtn.Location = New System.Drawing.Point(735, 475)
+        Me.ExitBtn.Name = "ExitBtn"
+        Me.ExitBtn.Size = New System.Drawing.Size(100, 80)
+        Me.ExitBtn.TabIndex = 7
+        Me.ExitBtn.Text = "Exit"
+        Me.ExitBtn.UseVisualStyleBackColor = True
         '
         'Form1
         '
@@ -32,10 +112,26 @@ Partial Class Form1
         Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
         Me.ClientSize = New System.Drawing.Size(1182, 653)
+        Me.Controls.Add(Me.ExitBtn)
+        Me.Controls.Add(Me.CommBtn)
+        Me.Controls.Add(Me.EnvCtrlBtn)
+        Me.Controls.Add(Me.VisitAptsBtn)
+        Me.Controls.Add(Me.DoorOpsBtn)
+        Me.Controls.Add(Me.WheelchairBtn)
+        Me.Controls.Add(Me.CallAssistanceBtn)
+        Me.Controls.Add(Me.NavSettingsBtn)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents NavSettingsBtn As Button
+    Friend WithEvents CallAssistanceBtn As Button
+    Friend WithEvents WheelchairBtn As Button
+    Friend WithEvents DoorOpsBtn As Button
+    Friend WithEvents VisitAptsBtn As Button
+    Friend WithEvents EnvCtrlBtn As Button
+    Friend WithEvents CommBtn As Button
+    Friend WithEvents ExitBtn As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,33 @@
 ﻿Public Class Form1
 
+    Private homePageNavigationSequence() As Button = {
+        Me.NavSettingsBtn,
+        Me.CallAssistanceBtn,
+        Me.WheelchairBtn,
+        Me.DoorOpsBtn,
+        Me.VisitAptsBtn,
+        Me.EnvCtrlBtn,
+        Me.CommBtn,
+        Me.ExitBtn
+    }
+
+    Private currentSelection As Button
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Do While True
+        'currentSelection = homePageNavigationSequence.
+        'Loop
+    End Sub
+
+    Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles MyBase.Paint
+        If currentSelection Is Nothing Then
+            currentSelection = Me.NavSettingsBtn
+        End If
+
+        Dim redPen As New Pen(Color.Red, 4)
+        Dim outlineDistance As Integer = 10
+        Dim rect As New Rectangle(currentSelection.Location.X - outlineDistance, currentSelection.Location.Y - outlineDistance, currentSelection.Width + (outlineDistance * 2), currentSelection.Height + (outlineDistance * 2))
+
+        e.Graphics.DrawRectangle(redPen, rect)
+    End Sub
 End Class
