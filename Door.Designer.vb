2bc57a00e7dd85b361e3f6d54882ee5baffa2f37
Mubariz Afzal <42556114+mubarizafzal@users.noreply.github.com>
2022-11-22 06:01:07 -0500

    add nav for Door and Elevator

--- Door.Designer.vb
+++ Door.Designer.vb
@@ -22,78 +22,76 @@ Partial Class Door
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Door))
-        Me.Door_lock = New System.Windows.Forms.Button()
+        Me.Door_Lock = New System.Windows.Forms.Button()
         Me.Door_Close = New System.Windows.Forms.Button()
         Me.Door_Unlock = New System.Windows.Forms.Button()
         Me.Door_Open = New System.Windows.Forms.Button()
-        Me.Door_Back = New System.Windows.Forms.Button()
-        Me.Door_Home = New System.Windows.Forms.Button()
+        Me.Close = New System.Windows.Forms.Button()
         Me.Building_Door = New System.Windows.Forms.RadioButton()
         Me.Apartment_Door = New System.Windows.Forms.RadioButton()
         Me.Hallway_Door = New System.Windows.Forms.RadioButton()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.SuspendLayout()
         '
-        'Door_lock
+        'Door_Lock
         '
-        Me.Door_lock.Location = New System.Drawing.Point(202, 314)
-        Me.Door_lock.Name = "Door_lock"
-        Me.Door_lock.Size = New System.Drawing.Size(75, 75)
-        Me.Door_lock.TabIndex = 3
-        Me.Door_lock.Text = "Lock"
-        Me.Door_lock.UseVisualStyleBackColor = True
+        Me.Door_Lock.Location = New System.Drawing.Point(371, 238)
+        Me.Door_Lock.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Door_Lock.Name = "Door_Lock"
+        Me.Door_Lock.Size = New System.Drawing.Size(90, 90)
+        Me.Door_Lock.TabIndex = 3
+        Me.Door_Lock.Text = "Lock"
+        Me.Door_Lock.UseVisualStyleBackColor = True
         '
         'Door_Close
         '
-        Me.Door_Close.Location = New System.Drawing.Point(660, 314)
+        Me.Door_Close.Location = New System.Drawing.Point(483, 346)
+        Me.Door_Close.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Door_Close.Name = "Door_Close"
-        Me.Door_Close.Size = New System.Drawing.Size(75, 75)
+        Me.Door_Close.Size = New System.Drawing.Size(90, 90)
         Me.Door_Close.TabIndex = 4
         Me.Door_Close.Text = "Close"
         Me.Door_Close.UseVisualStyleBackColor = True
         '
         'Door_Unlock
         '
-        Me.Door_Unlock.Location = New System.Drawing.Point(362, 314)
+        Me.Door_Unlock.Location = New System.Drawing.Point(483, 238)
+        Me.Door_Unlock.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Door_Unlock.Name = "Door_Unlock"
-        Me.Door_Unlock.Size = New System.Drawing.Size(75, 75)
+        Me.Door_Unlock.Size = New System.Drawing.Size(90, 90)
         Me.Door_Unlock.TabIndex = 5
         Me.Door_Unlock.Text = "Unlock"
         Me.Door_Unlock.UseVisualStyleBackColor = True
         '
         'Door_Open
         '
-        Me.Door_Open.Location = New System.Drawing.Point(516, 314)
+        Me.Door_Open.Location = New System.Drawing.Point(371, 346)
+        Me.Door_Open.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Door_Open.Name = "Door_Open"
-        Me.Door_Open.Size = New System.Drawing.Size(75, 75)
+        Me.Door_Open.Size = New System.Drawing.Size(90, 90)
         Me.Door_Open.TabIndex = 6
         Me.Door_Open.Text = "Open"
         Me.Door_Open.UseVisualStyleBackColor = True
         '
-        'Door_Back
+        'Close
         '
-        Me.Door_Back.Location = New System.Drawing.Point(97, 498)
-        Me.Door_Back.Name = "Door_Back"
-        Me.Door_Back.Size = New System.Drawing.Size(75, 75)
-        Me.Door_Back.TabIndex = 7
-        Me.Door_Back.Text = "Back"
-        Me.Door_Back.UseVisualStyleBackColor = True
-        '
-        'Door_Home
-        '
-        Me.Door_Home.Location = New System.Drawing.Point(798, 498)
-        Me.Door_Home.Name = "Door_Home"
-        Me.Door_Home.Size = New System.Drawing.Size(75, 75)
-        Me.Door_Home.TabIndex = 8
-        Me.Door_Home.Text = "Home"
-        Me.Door_Home.UseVisualStyleBackColor = True
+        Me.Close.Location = New System.Drawing.Point(749, 460)
+        Me.Close.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Close.Name = "Close"
+        Me.Close.Size = New System.Drawing.Size(110, 110)
+        Me.Close.TabIndex = 8
+        Me.Close.Text = "Close"
+        Me.Close.UseVisualStyleBackColor = True
         '
         'Building_Door
         '
         Me.Building_Door.AutoSize = True
-        Me.Building_Door.Location = New System.Drawing.Point(202, 188)
+        Me.Building_Door.Location = New System.Drawing.Point(142, 146)
+        Me.Building_Door.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Building_Door.Name = "Building_Door"
-        Me.Building_Door.Size = New System.Drawing.Size(129, 19)
+        Me.Building_Door.Size = New System.Drawing.Size(161, 24)
         Me.Building_Door.TabIndex = 9
         Me.Building_Door.Text = "Building Front Door"
         Me.Building_Door.UseVisualStyleBackColor = True
@@ -101,9 +99,10 @@ Partial Class Door
         'Apartment_Door
         '
         Me.Apartment_Door.AutoSize = True
-        Me.Apartment_Door.Location = New System.Drawing.Point(423, 188)
+        Me.Apartment_Door.Location = New System.Drawing.Point(418, 146)
+        Me.Apartment_Door.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Apartment_Door.Name = "Apartment_Door"
-        Me.Apartment_Door.Size = New System.Drawing.Size(111, 19)
+        Me.Apartment_Door.Size = New System.Drawing.Size(139, 24)
         Me.Apartment_Door.TabIndex = 10
         Me.Apartment_Door.Text = "Apartment Door"
         Me.Apartment_Door.UseVisualStyleBackColor = True
@@ -111,28 +110,35 @@ Partial Class Door
         'Hallway_Door
         '
         Me.Hallway_Door.AutoSize = True
-        Me.Hallway_Door.Location = New System.Drawing.Point(634, 188)
+        Me.Hallway_Door.Location = New System.Drawing.Point(691, 146)
+        Me.Hallway_Door.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Hallway_Door.Name = "Hallway_Door"
-        Me.Hallway_Door.Size = New System.Drawing.Size(96, 19)
+        Me.Hallway_Door.Size = New System.Drawing.Size(121, 24)
         Me.Hallway_Door.TabIndex = 11
         Me.Hallway_Door.Text = "Hallway Door"
         Me.Hallway_Door.UseVisualStyleBackColor = True
         '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
         'Door
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
-        Me.ClientSize = New System.Drawing.Size(968, 671)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ClientSize = New System.Drawing.Size(982, 753)
         Me.Controls.Add(Me.Hallway_Door)
         Me.Controls.Add(Me.Apartment_Door)
         Me.Controls.Add(Me.Building_Door)
-        Me.Controls.Add(Me.Door_Home)
-        Me.Controls.Add(Me.Door_Back)
+        Me.Controls.Add(Me.Close)
         Me.Controls.Add(Me.Door_Open)
         Me.Controls.Add(Me.Door_Unlock)
         Me.Controls.Add(Me.Door_Close)
-        Me.Controls.Add(Me.Door_lock)
+        Me.Controls.Add(Me.Door_Lock)
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Name = "Door"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Door"
@@ -140,13 +146,13 @@ Partial Class Door
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents Door_lock As Button
+    Friend WithEvents Door_Lock As Button
     Friend WithEvents Door_Close As Button
     Friend WithEvents Door_Unlock As Button
     Friend WithEvents Door_Open As Button
-    Friend WithEvents Door_Back As Button
-    Friend WithEvents Door_Home As Button
+    Friend WithEvents Close As Button
     Friend WithEvents Building_Door As RadioButton
     Friend WithEvents Apartment_Door As RadioButton
     Friend WithEvents Hallway_Door As RadioButton
+    Friend WithEvents Timer1 As Timer
 End Class
--- Elevator.vb
+++ Elevator.vb
@@ -1,8 +1,48 @@
 ﻿Imports System.Windows
 
 Public Class Elevator
-    Private Sub Elevator_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Private selectionSequence(2) As Button
+    Private ready As Boolean = False
+    Private selectionIndex As Integer = 0
+    Private redPen As New Pen(Color.FromArgb(255, 240, 0, 0), 4)
 
+    Private Sub Elevator_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.CallElevator.TabStop = False
+        Me.SelectFloor.TabStop = False
+        Me.Close.TabStop = False
+
+        Me.selectionSequence(0) = Me.CallElevator
+        Me.selectionSequence(1) = Me.SelectFloor
+        Me.selectionSequence(2) = Me.Close
+
+        Me.ready = True
+        Me.Invalidate()
+    End Sub
+
+    Private Sub Elevator_Paint(sender As Object, e As PaintEventArgs) Handles MyBase.Paint
+        If (ready) Then
+            Dim x As Integer = Me.selectionSequence(selectionIndex).Location.X - 5
+            Dim y As Integer = Me.selectionSequence(selectionIndex).Location.Y - 5
+            Dim width = Me.selectionSequence(selectionIndex).Size.Width + 10
+            Dim height = Me.selectionSequence(selectionIndex).Size.Height + 10
+            e.Graphics.DrawRectangle(redPen, x, y, width, height)
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Me.selectionIndex = (selectionIndex + 1) Mod selectionSequence.Length
+        Me.Invalidate()
+    End Sub
+
+    Private Sub Elevator_KeyPress(sender As Object, e As KeyPressEventArgs) Handles MyBase.KeyPress
+        If e.KeyChar = " " Then
+            If Me.selectionSequence(selectionIndex).Name = "Close" Then
+                ActiveForm.Close()
+            ElseIf Me.selectionSequence(selectionIndex).Name = "SelectFloor" Then
+                Dim NewWindow As New SelectFloor()
+                NewWindow.Show()
+            End If
+        End If
     End Sub
 
     Private Sub SelectFloor_Click(sender As Object, e As EventArgs) Handles SelectFloor.Click
@@ -14,11 +54,11 @@ Public Class Elevator
 
     End Sub
 
-    Private Sub Ele_back_Click(sender As Object, e As EventArgs) Handles Ele_back.Click
+    Private Sub Ele_back_Click(sender As Object, e As EventArgs)
 
     End Sub
 
-    Private Sub Ele_Home_Click(sender As Object, e As EventArgs) Handles Ele_Home.Click
+    Private Sub Ele_Home_Click(sender As Object, e As EventArgs) Handles Close.Click
 
     End Sub
 End Class
