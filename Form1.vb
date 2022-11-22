e0c638ce20d7a0c997bb95e3835c8760cb165d73
Edwin Lee <edwin1.lee@ryerson.ca>
2022-11-22 11:07:09 -0500

    add nav

--- Form1.vb
+++ Form1.vb
@@ -78,4 +78,44 @@ Public Class Form1
         Me.Invalidate()
     End Sub
 
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        selectionIndex = (selectionIndex + 1) Mod selectionSequence.Length
+        Me.Invalidate()
+    End Sub
+
+
+    Private Sub Elevator_Click(sender As Object, e As EventArgs) Handles Elevator.Click
+        Dim NewWindow As New Elevator()
+        NewWindow.Show()
+    End Sub
+
+    Private Sub Movement_Click(sender As Object, e As EventArgs) Handles Movement.Click
+        Dim NewWindow As New Movement()
+        NewWindow.Show()
+    End Sub
+
+    Private Sub Door_Click(sender As Object, e As EventArgs) Handles Door.Click
+        Dim NewWindow As New Door()
+        NewWindow.Show()
+    End Sub
+
+    Private Sub Apartment_Click(sender As Object, e As EventArgs) Handles Apartment.Click
+        Dim NewWindow As New Environment()
+        NewWindow.Show()
+    End Sub
+
+    Private Sub Entertainment_Click(sender As Object, e As EventArgs) Handles Entertainment.Click
+        Dim NewWindow As New Entertainment()
+        NewWindow.Show()
+    End Sub
+
+    Private Sub Visiting_Click(sender As Object, e As EventArgs) Handles Visiting.Click
+        Dim NewWindow As New Visiting()
+        NewWindow.Show()
+    End Sub
+
+    Private Sub CallAssist_Click(sender As Object, e As EventArgs) Handles CallAssist.Click
+        Dim NewWindow As New Assistance()
+        NewWindow.Show()
+    End Sub
 End Class
