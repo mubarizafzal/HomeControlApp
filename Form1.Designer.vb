a718d85f3477c7a03270b09ae5eebb5d71d50cf1
Mubariz Afzal <42556114+mubarizafzal@users.noreply.github.com>
2022-11-22 04:07:49 -0500

    Add navigation to home screen

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Movement = New System.Windows.Forms.Button()
         Me.CallAssist = New System.Windows.Forms.Button()
@@ -32,96 +33,115 @@ Partial Class Form1
         Me.Entertainment = New System.Windows.Forms.Button()
         Me.Bed = New System.Windows.Forms.Button()
         Me.Communication = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.SuspendLayout()
         '
         'Movement
         '
-        Me.Movement.Location = New System.Drawing.Point(166, 142)
+        Me.Movement.Location = New System.Drawing.Point(140, 120)
+        Me.Movement.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Movement.Name = "Movement"
-        Me.Movement.Size = New System.Drawing.Size(75, 75)
+        Me.Movement.Size = New System.Drawing.Size(110, 110)
         Me.Movement.TabIndex = 0
+        Me.Movement.TabStop = False
         Me.Movement.Text = "Movement"
         Me.Movement.UseVisualStyleBackColor = True
         '
         'CallAssist
         '
-        Me.CallAssist.Location = New System.Drawing.Point(349, 142)
+        Me.CallAssist.Location = New System.Drawing.Point(310, 120)
+        Me.CallAssist.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.CallAssist.Name = "CallAssist"
-        Me.CallAssist.Size = New System.Drawing.Size(75, 75)
+        Me.CallAssist.Size = New System.Drawing.Size(110, 110)
         Me.CallAssist.TabIndex = 1
+        Me.CallAssist.TabStop = False
         Me.CallAssist.Text = "Call For Assistance"
         Me.CallAssist.UseVisualStyleBackColor = True
         '
         'Door
         '
-        Me.Door.Location = New System.Drawing.Point(530, 142)
+        Me.Door.Location = New System.Drawing.Point(480, 120)
+        Me.Door.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Door.Name = "Door"
-        Me.Door.Size = New System.Drawing.Size(75, 75)
+        Me.Door.Size = New System.Drawing.Size(110, 110)
         Me.Door.TabIndex = 2
+        Me.Door.TabStop = False
         Me.Door.Text = "Door Controls"
         Me.Door.UseVisualStyleBackColor = True
         '
         'Elevator
         '
-        Me.Elevator.Location = New System.Drawing.Point(702, 142)
+        Me.Elevator.Location = New System.Drawing.Point(650, 120)
+        Me.Elevator.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Elevator.Name = "Elevator"
-        Me.Elevator.Size = New System.Drawing.Size(75, 75)
+        Me.Elevator.Size = New System.Drawing.Size(110, 110)
         Me.Elevator.TabIndex = 3
+        Me.Elevator.TabStop = False
         Me.Elevator.Text = "Elevator"
         Me.Elevator.UseVisualStyleBackColor = True
         '
         'Apartment
         '
-        Me.Apartment.Location = New System.Drawing.Point(166, 301)
+        Me.Apartment.Location = New System.Drawing.Point(140, 280)
+        Me.Apartment.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Apartment.Name = "Apartment"
-        Me.Apartment.Size = New System.Drawing.Size(75, 75)
+        Me.Apartment.Size = New System.Drawing.Size(110, 110)
         Me.Apartment.TabIndex = 4
         Me.Apartment.Text = "Apartment Control"
         Me.Apartment.UseVisualStyleBackColor = True
         '
         'Visiting
         '
-        Me.Visiting.Location = New System.Drawing.Point(349, 301)
+        Me.Visiting.Location = New System.Drawing.Point(310, 280)
+        Me.Visiting.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Visiting.Name = "Visiting"
-        Me.Visiting.Size = New System.Drawing.Size(75, 75)
+        Me.Visiting.Size = New System.Drawing.Size(110, 110)
         Me.Visiting.TabIndex = 5
         Me.Visiting.Text = "Visiting Apartment"
         Me.Visiting.UseVisualStyleBackColor = True
         '
         'Entertainment
         '
-        Me.Entertainment.Location = New System.Drawing.Point(530, 301)
+        Me.Entertainment.Location = New System.Drawing.Point(480, 280)
+        Me.Entertainment.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Entertainment.Name = "Entertainment"
-        Me.Entertainment.Size = New System.Drawing.Size(75, 75)
+        Me.Entertainment.Size = New System.Drawing.Size(110, 110)
         Me.Entertainment.TabIndex = 6
         Me.Entertainment.Text = "Entertaiment"
         Me.Entertainment.UseVisualStyleBackColor = True
         '
         'Bed
         '
-        Me.Bed.Location = New System.Drawing.Point(702, 301)
+        Me.Bed.Location = New System.Drawing.Point(650, 280)
+        Me.Bed.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Bed.Name = "Bed"
-        Me.Bed.Size = New System.Drawing.Size(75, 75)
+        Me.Bed.Size = New System.Drawing.Size(110, 110)
         Me.Bed.TabIndex = 7
         Me.Bed.Text = "Bed Controls"
         Me.Bed.UseVisualStyleBackColor = True
         '
         'Communication
         '
-        Me.Communication.Location = New System.Drawing.Point(166, 440)
+        Me.Communication.Location = New System.Drawing.Point(140, 440)
+        Me.Communication.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Communication.Name = "Communication"
-        Me.Communication.Size = New System.Drawing.Size(75, 75)
+        Me.Communication.Size = New System.Drawing.Size(110, 110)
         Me.Communication.TabIndex = 8
-        Me.Communication.Text = "Communication"
+        Me.Communication.Text = "Communic -ation"
         Me.Communication.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
-        Me.ClientSize = New System.Drawing.Size(968, 671)
+        Me.ClientSize = New System.Drawing.Size(1106, 895)
         Me.Controls.Add(Me.Communication)
         Me.Controls.Add(Me.Bed)
         Me.Controls.Add(Me.Entertainment)
@@ -131,7 +151,6 @@ Partial Class Form1
         Me.Controls.Add(Me.Door)
         Me.Controls.Add(Me.CallAssist)
         Me.Controls.Add(Me.Movement)
-        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Form1"
@@ -148,4 +167,5 @@ Partial Class Form1
     Friend WithEvents Entertainment As Button
     Friend WithEvents Bed As Button
     Friend WithEvents Communication As Button
+    Friend WithEvents Timer1 As Timer
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,22 +1,71 @@
-﻿Public Class Form1
-    Private Sub Elevator_Click(sender As Object, e As EventArgs) Handles Elevator.Click
-        Dim NewWindow As New Elevator()
-        NewWindow.Show()
+﻿Imports System.Threading
+
+Public Class Form1
+
+    Private selectionSequence(8) As Button ' 8 gives 9 elements (0-indexed)
+    Private ready As Boolean = False
+    Private selectionIndex As Integer = 0
+    Private redPen As New Pen(Color.FromArgb(255, 240, 0, 0), 4)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Disable usage of tab
+        Me.Movement.TabStop = False
+        Me.Door.TabStop = False
+        Me.CallAssist.TabStop = False
+        Me.Elevator.TabStop = False
+        Me.Visiting.TabStop = False
+        Me.Apartment.TabStop = False
+        Me.Bed.TabStop = False
+        Me.Communication.TabStop = False
+        Me.Entertainment.TabStop = False
+        ' Declare sequence
+        Me.selectionSequence(0) = Me.Movement
+        Me.selectionSequence(1) = Me.CallAssist
+        Me.selectionSequence(2) = Me.Door
+        Me.selectionSequence(3) = Me.Elevator
+        Me.selectionSequence(4) = Me.Apartment
+        Me.selectionSequence(5) = Me.Visiting
+        Me.selectionSequence(6) = Me.Entertainment
+        Me.selectionSequence(7) = Me.Bed
+        Me.selectionSequence(8) = Me.Communication
+
+        Me.ready = True
+        Me.Invalidate()
     End Sub
 
-    Private Sub Movement_Click(sender As Object, e As EventArgs) Handles Movement.Click
-        Dim NewWindow As New Movement()
-        NewWindow.Show()
+    Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles MyBase.Paint
+        If (ready) Then
+            Dim x As Integer = Me.selectionSequence(selectionIndex).Location.X - 5
+            Dim y As Integer = Me.selectionSequence(selectionIndex).Location.Y - 5
+            Dim width = Me.selectionSequence(selectionIndex).Size.Width + 10
+            Dim height = Me.selectionSequence(selectionIndex).Size.Height + 10
+            e.Graphics.DrawRectangle(redPen, x, y, width, height)
+        End If
     End Sub
 
-    Private Sub Door_Click(sender As Object, e As EventArgs) Handles Door.Click
-        Dim NewWindow As New Door()
-        NewWindow.Show()
+    Private Sub Form1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles MyBase.KeyPress
+        If e.KeyChar = " " Then
+            Dim currentSelection As Button = selectionSequence(selectionIndex)
+            Select Case currentSelection.Name
+                Case Me.Elevator.Name
+                    Dim NewWindow As New Elevator()
+                    NewWindow.Show()
+                Case Me.Movement.Name
+                    Dim NewWindow As New Movement()
+                    NewWindow.Show()
+                Case Me.Door.Name
+                    Dim NewWindow As New Door()
+                    NewWindow.Show()
+                Case Me.Apartment.Name
+                    Dim NewWindow As New Environment()
+                    NewWindow.Show()
+            End Select
+        End If
     End Sub
 
-    Private Sub Apartment_Click(sender As Object, e As EventArgs) Handles Apartment.Click
-        Dim NewWindow As New Environment()
-        NewWindow.Show()
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        selectionIndex = (selectionIndex + 1) Mod selectionSequence.Length
+        Me.Invalidate()
     End Sub
 
     Private Sub Entertainment_Click(sender As Object, e As EventArgs) Handles Entertainment.Click
