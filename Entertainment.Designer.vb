f53a444269a286374492915b4cce5d1622b23061
Mubariz Afzal <42556114+mubarizafzal@users.noreply.github.com>
2022-11-22 12:53:28 -0500

    Add navigation for Entertainment and TV

--- Entertainment.Designer.vb
+++ Entertainment.Designer.vb
@@ -22,28 +22,31 @@ Partial Class Entertainment
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Entertainment))
         Me.Ent_TV = New System.Windows.Forms.Button()
         Me.Ent_Radio = New System.Windows.Forms.Button()
         Me.Ent_Games = New System.Windows.Forms.Button()
-        Me.Ent_Back = New System.Windows.Forms.Button()
-        Me.Ent_Home = New System.Windows.Forms.Button()
+        Me.Close = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.SuspendLayout()
         '
         'Ent_TV
         '
-        Me.Ent_TV.Location = New System.Drawing.Point(263, 281)
+        Me.Ent_TV.Location = New System.Drawing.Point(249, 135)
+        Me.Ent_TV.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Ent_TV.Name = "Ent_TV"
-        Me.Ent_TV.Size = New System.Drawing.Size(75, 75)
+        Me.Ent_TV.Size = New System.Drawing.Size(86, 100)
         Me.Ent_TV.TabIndex = 0
         Me.Ent_TV.Text = "TV"
         Me.Ent_TV.UseVisualStyleBackColor = True
         '
         'Ent_Radio
         '
-        Me.Ent_Radio.Location = New System.Drawing.Point(437, 281)
+        Me.Ent_Radio.Location = New System.Drawing.Point(447, 135)
+        Me.Ent_Radio.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Ent_Radio.Name = "Ent_Radio"
-        Me.Ent_Radio.Size = New System.Drawing.Size(75, 75)
+        Me.Ent_Radio.Size = New System.Drawing.Size(86, 100)
         Me.Ent_Radio.TabIndex = 1
         Me.Ent_Radio.Text = "Radio"
         Me.Ent_Radio.UseVisualStyleBackColor = True
@@ -51,42 +54,41 @@ Partial Class Entertainment
         'Ent_Games
         '
         Me.Ent_Games.Enabled = False
-        Me.Ent_Games.Location = New System.Drawing.Point(621, 281)
+        Me.Ent_Games.Location = New System.Drawing.Point(638, 135)
+        Me.Ent_Games.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Ent_Games.Name = "Ent_Games"
-        Me.Ent_Games.Size = New System.Drawing.Size(75, 75)
+        Me.Ent_Games.Size = New System.Drawing.Size(86, 100)
         Me.Ent_Games.TabIndex = 2
         Me.Ent_Games.Text = "Games"
         Me.Ent_Games.UseVisualStyleBackColor = True
         '
-        'Ent_Back
+        'Close
         '
-        Me.Ent_Back.Location = New System.Drawing.Point(97, 500)
-        Me.Ent_Back.Name = "Ent_Back"
-        Me.Ent_Back.Size = New System.Drawing.Size(75, 75)
-        Me.Ent_Back.TabIndex = 3
-        Me.Ent_Back.Text = "Back"
-        Me.Ent_Back.UseVisualStyleBackColor = True
+        Me.Close.Location = New System.Drawing.Point(749, 449)
+        Me.Close.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Close.Name = "Close"
+        Me.Close.Size = New System.Drawing.Size(110, 110)
+        Me.Close.TabIndex = 4
+        Me.Close.Text = "Close"
+        Me.Close.UseVisualStyleBackColor = True
         '
-        'Ent_Home
+        'Timer1
         '
-        Me.Ent_Home.Location = New System.Drawing.Point(798, 500)
-        Me.Ent_Home.Name = "Ent_Home"
-        Me.Ent_Home.Size = New System.Drawing.Size(75, 75)
-        Me.Ent_Home.TabIndex = 4
-        Me.Ent_Home.Text = "Home"
-        Me.Ent_Home.UseVisualStyleBackColor = True
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
         '
         'Entertainment
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
-        Me.ClientSize = New System.Drawing.Size(968, 671)
-        Me.Controls.Add(Me.Ent_Home)
-        Me.Controls.Add(Me.Ent_Back)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ClientSize = New System.Drawing.Size(982, 753)
+        Me.Controls.Add(Me.Close)
         Me.Controls.Add(Me.Ent_Games)
         Me.Controls.Add(Me.Ent_Radio)
         Me.Controls.Add(Me.Ent_TV)
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Name = "Entertainment"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Entertainment"
@@ -97,6 +99,6 @@ Partial Class Entertainment
     Friend WithEvents Ent_TV As Button
     Friend WithEvents Ent_Radio As Button
     Friend WithEvents Ent_Games As Button
-    Friend WithEvents Ent_Back As Button
-    Friend WithEvents Ent_Home As Button
+    Friend WithEvents Close As Button
+    Friend WithEvents Timer1 As Timer
 End Class
